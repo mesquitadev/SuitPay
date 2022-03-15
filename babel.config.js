86e3807db14e2e0b1ab5d8df91bb1b7a8228fdbb
Victor Mesquita <mesquitadev@gmail.com>
2022-03-14 22:50:14 -0300

    internal: add absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@globals': './src/global',
+          '@pages': './src/pages',
+          '@routes': './src/routes',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
