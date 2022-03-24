8ffc7a62ee26d4813f2484e33c5e79b98506b2d6
Victor Mesquita <mesquitadev@gmail.com>
2022-03-24 11:23:13 -0300

    components: create dynamic exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export {default as Card} from './Card';
+export {default as InputForm} from './InputForm';
+export {default as InputPin} from './InputPin';
+export {default as Button} from './Button';
+export {default as ButtonLink} from './ButtonLink';
+export {default as PIcon} from './Icon';
