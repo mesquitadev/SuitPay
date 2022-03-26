4139c685d06db29e4d229f6f0c8961fcf5bb8da4
Victor Mesquita <mesquitadev@gmail.com>
2022-03-25 22:57:27 -0300

    pages: remove statusbar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {StatusBar, View} from 'react-native';
+import {View} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 import theme from '@globals/styles/theme';
 import Routes from '@routes/index';
@@ -19,10 +19,6 @@ function App() {
               flex: 1,
             }}>
             <NavigationContainer>
-              <StatusBar
-                barStyle="light-content"
-                backgroundColor={theme.colors.primary}
-              />
               <AppProvider>
                 <Routes />
               </AppProvider>
