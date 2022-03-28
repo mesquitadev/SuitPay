5fd08dd094a6cebcbfc9c1c27e1872e238d87699
Victor Mesquita <mesquitadev@gmail.com>
2022-03-27 23:19:32 -0300

    components: refactor button and add SS background

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,4 @@
 declare module '*.png';
 declare module 'react-native-smooth-pincode-input';
 declare module 'react-native-svg-icon';
+declare module 'react-native-image-gradient';
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {RectButtonProps} from 'react-native-gesture-handler';
 
+import {ActivityIndicator} from 'react-native';
 import {Container, Title} from './styles';
 
 interface Props extends RectButtonProps {
   title: string;
   onPress: () => void;
+  loading?: boolean;
 }
 
-export default function Button({title, onPress, ...rest}: Props) {
+export default function Button({title, onPress, loading, ...rest}: Props) {
   return (
     <Container onPress={onPress} {...rest}>
-      <Title>{title}</Title>
+      {loading ? <ActivityIndicator color="#0C072D" /> : <Title>{title}</Title>}
     </Container>
   );
 }
